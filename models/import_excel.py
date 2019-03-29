fb21276f32e8405b7ad11502c13952171fec2bb6
Roxas <roxas.kh.2019@gmail.com>
2019-03-29 10:44:37 -0500

    se cargo la tabla de persona desde excel

--- models/import_excel.py
+++ models/import_excel.py
@@ -25,16 +25,29 @@ class ImportExcel(models.Model):
     )
 
     def importar_excel(self):
-        filename = 'simple.xls'
+        filename = 'personas.xls'
         ROOT = os.path.abspath(os.sep)
         _logger.info('ROOT: {0}'.format(ROOT))
         filepath = os.path.join('/opt/odoo/project_live_sessions/pruebas/models/', filename)
         wb = open_workbook(filepath)
+        self.personas -= self.personas
+        personas = []
         for s in wb.sheets():
             _logger.info('Sheet: {0}'.format(s.name))
             for row in range(s.nrows):
                 values = []
+                persona = {}
+                i = 1
                 for col in range(s.ncols):
                     values.append(s.cell(row, col).value)
+                    if i == 1:
+                        persona['nombre'] = s.cell(row, col).value
+                    if i == 2:
+                        persona['apellido_paterno'] = s.cell(row, col).value
+                    if i == 3:
+                        persona['apellido_materno'] = s.cell(row, col).value
+                    i += 1
                 _logger.info(','.join(values))
+                personas += [(0, 0, persona)]
+        self.personas = personas
         return
